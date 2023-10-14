72f43d67bd58249c7a69e79ec192553121547b2b
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-10-15 00:21:40 +0400

    homeworks

--- Day 5/classwork/Day5.py
+++ Day 5/classwork/Day5.py
@@ -1,12 +1,51 @@
-#age = int(input(15))
-#limit = 18
-#height = int(input(129))
+# name = input() 
+# print("welcome, " + name)
 
-#if age > limit or height > 180:
-    #print("yes")
-#elif age < limit:height > 180
-    #print("you are kiddo")
-#else:
-    #print("dabali xar")
-name = "dati" 
-print (name)
+# bill = int(input()) 
+
+# tip = bill * 20 / 100
+# print(tip)
+
+print(4 == 3)
+
+print("hello" == "hello")
+
+True and False # თრუ და ფოლსი ყოველთვის იწერება ესე (True and False) ანუ დიდი ასოთი იწყება ყოველთვის 
+
+print(2!=3) #არ უდრის იწერება ძახილის ნიშნით და ტოლობით (!=)
+
+
+x = 7
+
+print(x != 8)
+print(x > 5)
+print(x < 2)
+print(x >= 7) #მეტია ან ტოლი
+print(x <= 7) #ნაკლებია ან ტოლი
+
+print("a" > "b") # ანბანში რომელიც უფრო მერე მოდის ის იმარჯვებს
+print("Bob" > "Dave")
+
+x = (7 > 5)
+print(int(x)) #int True gamova ertiani anu True
+
+# 1 = True
+# 0 = False
+
+# if goashiswavlobs:
+#     print("warmateba_garantirebulia")
+
+
+user_num =int(input("enter any numer: "))
+if user_num >10:
+    print("მეტია ათზე")
+elif user_num ==10:
+    print("უდრის 10-ს")
+else:
+    print("ნაკლებია ათზე")
+
+#მომხმარებლის ნიშნებისგან გამოანგარიშება საშვალო არითმეტიკული და თუ ცხრაზე მეტი იქნება 
+#დაუპრინტეთ გილოცავ მატრიცელო შენ გადმოგეცა 100 ლარიანი ბანძი ტოსტერი რისთვისაც შენ შეალიე მთელი სიცოცხლე
+#თუ ეს იქნება 5ზე ნაკლები დაუპრინტე გილოცავ გაექეცი მატრიცას
+#თუ იქნება 5დან 9მდე დაუპრინტე (იუ არ ნორმი( you are norm)
+#თუ იქნება 10ზე მეტი ან 0ზე ნაკლები დაუპრინტე there is somethinh wrong with you
---
+++ Day 7/classwork/Day7.py
@@ -0,0 +1,69 @@
+# print(5 % 2) # გამოითვლის ნაშთს პირდაპირ არ გაყოფს და არ დაწერს 2.5 (/)-(%) დაწერს რომ ნაშთი დარჩა 1 ანუ
+# #even ლუწი
+# #odd კენტი 
+# #f/str= ცვლადის შეყვანა ცვლადში
+
+# number_count = 1
+
+# while number_count <= 30:
+#     if number_count % 2 == 1:
+#         print(str(number_count) + " odd")
+#     else:
+#         print(str(number_count) + " is even")
+#     number_count += 1
+
+#     print("finished")
+
+
+# i = 2
+# while i < 30:
+#     print(i,"კენტია")
+#     print(i+1,"ლუწია")
+#     i += 2
+
+#     i = 10
+#     # i = i+1
+#     i +=1
+#     print (i) 
+
+# i = 2
+# while i < 20:
+#     print(i+2, "ლუწია")
+#     i += 2
+
+# sum = 
+# i = 0
+# while i < 100:
+#     print(i+2, "ლუწია")
+#     i +=2
+# print(sum)
+
+sum = 0
+x = 100
+while x > 0:
+    sum += x
+    x -= 2
+
+print(sum)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
