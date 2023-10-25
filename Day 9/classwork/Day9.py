dd7fa7c96cb50528c7ab9011e834f72c61221a26
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-10-25 23:32:11 +0400

    homeworks

---
+++ Day 9/classwork/Day9.py
@@ -0,0 +1,204 @@
+# print ("nika")
+
+# a=10
+# b=20
+
+
+# c = a + b 
+
+# for i in range (10):
+#     print(c)
+
+
+# # forward(100)
+# # left(90)
+# # forward(100)
+# # left(90)
+# # forward(100)
+# # left(90)
+# # forward(100)
+# # left(90)
+
+
+
+
+# from turtle import * 
+
+# speed(4)
+
+# for i in range(4):
+#     forward(100)
+#     left(90)
+
+# penup()
+# goto(0,200)
+# pendown()
+
+# for i in range(4):
+#     forward(100)
+#     left(90)
+
+# penup()
+# goto(-200,200)
+# pendown()
+
+# for i in range(4):
+#     forward(100)
+#     left(90)
+
+# penup()
+# goto(-200,0)
+# pendown()
+
+
+# for i in range(4):
+#     forward(100)
+#     left(90)
+
+
+
+
+
+
+# exitonclick()
+
+
+
+# for i in range(4):
+#     print("nika")
+
+
+# for i in range(4):
+#     print(i , "nika")
+
+
+
+
+
+
+
+
+
+# def bichebtan_misalmeba():
+#     print("hello nika")
+#     print("hello gio")
+#     print("hello luka")
+#     print("hello dati")
+
+# print("dila")
+# bichebtan_misalmeba()
+
+# print("shuadge")
+# bichebtan_misalmeba()
+
+# print("sagamo")
+# bichebtan_misalmeba()
+
+
+# def draw_squae():
+#     for i in range(4):
+#         forward(100)
+#         left(90)
+
+# def misalmeba(saxeli, age):
+#     print("mogesalmebi " + saxeli)
+#     print("sheni asakia " , age)
+
+# #პარამეტრი - ზოგადი სახელი
+# #არგუმენტი - კონკრეტული სახელი მაგ(ნიკა)
+# misalmeba("nika", 50)
+# misalmeba("dati", 30)
+# misalmeba("datuna", 20)
+# misalmeba("datucha", 100)
+# misalmeba("saba", 32)
+
+
+
+# from turtle import * 
+
+# def draw_square():
+#     for i in range(4):
+#         forward(100)
+#         left(90)
+
+# def kalmis_wageba(x,y):
+#     penup()
+#     goto(x,y)
+#     pendown() 
+
+# draw_square()
+# kalmis_wageba(0,200)
+
+# draw_square()
+# kalmis_wageba(-200,200)
+
+# #third square
+# draw_square()
+# kalmis_wageba(-200,0)
+
+# #fourth square
+# draw_square()
+
+# exitonclick()
+
+
+
+# number = list(range(10))
+# print(number)
+
+
+# numbers = list(range(5, 20, 2))
+# print(numbers)
+
+
+# nums = list(range(3, 15, 3))
+
+# print(nums[2])
+
+
+
+# x = list(range(7, 3, -2))
+# print(x)
+
+
+
+
+              #dan atamde
+# x = list(range(5, 10, 2))
+# print(x)
+
+# x = list(range(0, 100))
+# print(x[4])
+
+
+# # square = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# # print(square[2:6])
+# # print(square[3:8])
+# # print(square[0:1])
+#    #მაგ(2 დან 6 მდე [2:6])
+
+# # square = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# # print(square[:7])
+
+# #[7:] anu 7dan 81
+
+# square = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(square[::2]) #dasawyisidan bolomde yoveli meore
+# print(square[2:8:3])#2dan 8mde yoveli mesame
+
+#[1::4] ertidan daiwyebs,bolomde wava da 4-s gamotovebs
+
+# square = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+# print(square[1:-1]) #ukugma iwyeba atvla mag -1 = 81 -2 = 64 da assh
+
+#square = [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+#            -7 -6 -5  -6  -5  -4  -3  -2  -1
+
+
+# nums = [5, 42, 7, 1, 0]
+# res = nums[::-1] #::-1 არის სიის შეტრიალება
+# print(res)
+
+
+
+
