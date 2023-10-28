454855f3a27fe4095c86dbbe52d3ba1f0a0306c8
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-10-29 00:32:47 +0400

    homework

---
+++ Day 10/homeworks/Day10_ages.py
@@ -0,0 +1,23 @@
+# x = [2, 4, 6, 2, 4, 7, 2, 9]
+# print(x.count(2)) 
+# x.remove(4) 
+# x.remove(4)
+# print(x)
+# x.reverse() 
+# print(x)
+
+
+# family = ["tamuna", "saba", "mate"]
+# full_sentence = "My moms name is: {}, My brothers name is: {}, My name is: {}".format(family[0],family[1],family[2])
+# print(full_sentence)  დაემატოს ასაკები და დაიპრინტოს თითოეული რამდენი წლის არის ერთ გრძელ წინადადებად ხოლო მეორე დაპრინტვაზე დაიპრინტოს რამდენი წლის გახდნენ 10 წელში
+
+
+family = ["tatuli", "reziko", "dati",]
+ages = [36, 36, 4]
+years = [46, 46, 25]
+fam = "My moms name is: {}, My brothers name is: {}, My name is: {}".format(family[0],family[1],family[2])
+print(fam)
+age ="My moms name is: {} and her age is: {}, My brothers name is: {} and him age is: {}, My name is: {} and my age is: {}.".format(family[0],ages[1],family[1],ages[0],family[2],ages[2])
+print(age)
+years_10 = "My moms name is: {} and her age is: {}, My brothers name is: {} and him age is: {}, My name is: {} and my age is: {}.".format(family[0],years[1],family[1],years[0],family[2],years[2])
+print(years_10)
---
+++ Day 10/homeworks/list.py
@@ -0,0 +1,9 @@
+x = [2, 4, 6, 2, 4, 7, 2, 9]
+print(x.count(2)) 
+x.remove(4) 
+x.remove(4)
+print(x)
+x.reverse() 
+print(x)
+
+
