fec7f6e96eeaaf35250946261516b19d5d083161
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-11-01 23:18:53 +0400

    homework

---
+++ Day 11/classwork/Day11.py
@@ -0,0 +1,70 @@
+# x = [2, 4, 6, 2, 4, 7, 2, 9]
+
+# for i in range(2):
+#     x.remove(4)
+#     print(x)
+
+
+# old_list = [2, 4, 6, 2, 4, 7, 2, 9]
+# new_list = set(old_list) #monacemebi ar meordeba (aqprbs monacemebidan duplikatebs)
+# print(new_list)
+
+
+# old_list = [2, 4, 6, 2, 4, 7, 2, 9]
+# new_list = []
+# for num in old_list:
+#     if num != 4:
+#         new_list.append(num)
+#                #chamteba
+# print(new_list)
+
+
+# x = ",".join(["spam", "eggs", "ham"])
+# print(x)
+
+# x = "ZZ".join(["spam", "eggs", "ham"])
+# print(x)
+
+# str = "some text goes here"
+# x = str.split(' ')
+# print(x)
+
+# str = "some text goes here"
+# x = str.split('o')#sadac iqneba o yvelas gaxlichavs
+# print(x)
+
+#split -დაყოფა
+
+# x = "hello me"
+# print(x.replace("me", "world")) #შეცვლის ME-ს World-ად #მეთოდი
+
+#def aris keyowoard romlitac iqmneba funqcia
+#kvardatis_fartobi - funqciiis saxeli
+#(gverdis_sigrdze) - funqciis parametri
+
+
+
+#def - keywoard რომლითაც იქმნება ფუნქცია
+#kvadratis_fartobi - ფუნქციის სახელი
+#(gverdis_sigrdze) - ფუნქციის პარამეტრი
+
+# def kvadratis_fartobi(gverdis_sigrdze):
+#     print(gverdis_sigrdze * gverdis_sigrdze)
+
+# #(8) არის ფუნქციის არგუმენტი
+# kvadratis_fartobi(8)
+# kvadratis_fartobi(12)
+
+
+#keyword to create function
+#square_area - name of function
+#lenght_of_side - parameter of function
+def square_area(lenght_of_side):
+    print(lenght_of_side**2)
+square_area(float(input()))
+
+
+
+
+
+
---
+++ projects/test.py
@@ -0,0 +1,106 @@
+# season =int(input("რამდენი გოლი გაიტანე ამ სეზონში"))
+
+# if season > 15:
+#     print("კარგი ფეხბურთელი ხარ")
+# elif season < 5:
+#     print("გადი ფეხბურთიდან სანამ დროა")
+# else:
+#     print("კარგი სეზონი გქონდა")
+
+
+
+# square = [1, 3, 6, 9, 12, 16, 23, 44]
+
+# print(square[2::3])
+
+
+# for i in range (15, 100, 15):
+#     print(i)
+
+# i = 1
+
+# while i < 20:
+#     if i % 2 == 0:
+#         print(i,  "uwia")
+#     if i % 2 == 1:
+#         print(i,  "entia")
+#     i += 1
+    
+# i = 1
+
+# while i < 20:
+#     print(i)
+#     i += 1
+
+# square = [1, 3, 6, 9, 12, 16, 23, 44]
+
+# print(square[6])
+# print(square[:3])
+# print(square[3:])
+
+# number = list(range(20))
+# print(number)
+
+
+
+
+# footballer_age = int(input("რამდენი წლის ხარ?"))
+
+# if footballer_age <15:
+#     print("ჯერ პატარა ხარ")
+# elif footballer_age >32:
+#     print("ნელ ნელა კარიერის დასრულებაზე უნდა იფიქრო")
+# else:
+#     print("კარგი ასაკი გაქვს გააგრძელე თამაში")
+
+# footballer_age = int(input("რამდენი წლის ხარ?"))
+
+# if footballer_age <18:
+#     print("ჯერ პატარა ხარ")
+# elif footballer_age >32:
+#     print("ნელ ნელა კარიერის დასრულებაზე უნდა იფიქრო")
+# else:
+#     print("კარგი ასაკი გაქვს გააგრძელე თამაში")
+
+# goal_season = int(input("რამდენი გოლი გაიტანე ამ სეზონში?"))
+
+# if goal_season < 5:
+#     print("ცუდი შედეგია ბევრი უნდა ივარჯიშო")
+# elif goal_season > 15:
+#     print("კარგი შედეგია გააგრძელე")
+# else:
+#     print("ნორმ მეტი შეგიძლია")
+
+# asisst_season = int(input("რამენი საგოლე გადაცემა გააკეთე ამ სეზონში?"))
+
+# if asisst_season < 5:
+#     print("თუ ნახევარ მცველი ან შემტევი ხარ მეტი უნდა ივარჯიშო იმიტორო არ არის კარგი შედეგი")
+# elif asisst_season > 10:
+#     print("ყოჩახ კარგი შედეგი ასე განაგრძე")
+# else:
+#     ("მეტი შეგიძლია")
+
+
+
+from turtle import * 
+
+speed(55)
+color("green")
+bgcolor("black")
+x = 200
+while x > 0:
+    left(85)
+    forward(x * 2)
+
+    x= x - 1
+
+
+
+
+
+
+exitonclick()
+
+
+
+   
