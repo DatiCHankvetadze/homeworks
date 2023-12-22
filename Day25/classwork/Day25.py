b0f481ba270572671d09e183821dc2e165628e09
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-12-22 18:06:56 +0400

    sololearn homework

---
+++ Day25/classwork/Day25.py
@@ -0,0 +1,10 @@
+# def string_to_array(s):
+#     return s.split(' ')
+
+# def positive_sum(arr):
+#     sum_of_positives = 0
+#     for num in arr:
+#         if num > 0:
+#             sum_of_positives += num
+#     print(sum_of_positives) 
+
