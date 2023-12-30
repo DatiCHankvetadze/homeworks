e41a5300a9b513e2a3c11e81e5c239878ed2ea27
DatiChankvetadze <Datichankvetadze@gmail.com>
2023-12-30 22:32:22 +0400

    datis trip phyton and santa klaus

---
+++ Day28/Day28.py
@@ -0,0 +1,46 @@
+def  addition(x,y):
+    return x + y
+
+def  multiplication(x,y):
+    return x * y
+
+def  division(x,y):
+    return x / y
+
+def deduction(x,y):
+    return x - y
+
+for num in range(1,100):
+    print(num)
+
+if  addition + num:
+    print(sum) 
+
+#მხოლოდ ჯამი გამომივიდა 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
