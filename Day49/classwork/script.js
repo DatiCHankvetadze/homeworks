07740e87b55def68931e3942c0a36046a4a20396
DatiChankvetadze <Datichankvetadze@gmail.com>
2024-03-30 20:25:41 +0400

    homework

---
+++ Day49/classwork/script.js
@@ -0,0 +1,9 @@
+// const PI = 3.14;
+// const R = 10;
+
+// console.log(PI * 2 * R)
+// console.log((2 * PI) * R)
+
+document.getElementById("e1");
+
+document.getElementById('h2').textContent;
---
+++ Day51/classwork/script.js
@@ -0,0 +1,24 @@
+// let goa = document.getElementById("h1").textContent="Goa"
+// alert(goa)
+
+// {
+//     let x = "Game Over";
+//     alert(x);
+//     console.log(x);
+// }
+
+// function doSomething() {
+//   let x = "Game Over";
+//   alert(x);
+//   console.log(x);
+// }
+
+
+
+function sayHello() {
+console.log("Hello, world!");
+}
+sayHello();
+
+
+
